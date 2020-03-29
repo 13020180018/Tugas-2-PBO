d48ddd9a0eae8b3dad0d1bbfe5bca51e784a083d
13020180018 <62000318+13020180018@users.noreply.github.com>
2020-03-29 19:09:11 +0700

    Add files via upload

---
+++ Tugas 2/tiwi.java
@@ -0,0 +1,17 @@
+/*	Nama 		: Nur aulia pertiwi
+	kelas 		: B1
+	waktu 		: 23.10
+	hari/tanggal 	: senin/16 maret 2020
+*/
+public class tiwi{
+public static void main(String[] args){
+String nim 	="13020180018";
+String nama 	="Nur aulia pertiwi";
+String jurusan	="Teknik Informatika";
+String fakultas	="Ilmu Komputer";
+System.out.println("nim saya : " + nim);
+System.out.println("nama saya : " + nama);
+System.out.println("jurusan saya : " + jurusan);
+System.out.println("fakultas saya : " + fakultas);
+}
+}
